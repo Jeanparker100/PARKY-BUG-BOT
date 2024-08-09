431ce89f517966901cd84a18cdc2eec32194335e
KillerBee970 <kbee85915@gmail.com>
2024-08-09 02:52:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,19 +11,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
-global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
+global.ownernomer = "18298621148"
+global.ownername = "𖤐𝗦𝗲́𝗻𝗮𝘁𝗲𝘂𝗿✍️𝗕𝗹𝗮𝗰𝗸✞𝗞𝗶𝗹𝗹𝗲𝗿"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
-global.ownernumber = '22898133388'  //creator number
-global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
+global.ownernumber = '18298621148'  //creator number
+global.ownername = '𖤐𝗦𝗲́𝗻𝗮𝘁𝗲𝘂𝗿✍️𝗕𝗹𝗮𝗰𝗸✞𝗞𝗶𝗹𝗹𝗲𝗿' //owner name
 global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
 global.packname = '¹⁴³ ᴬˡᵉᵐᵃᵖ 🧸💜'
-global.author = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛'
+global.author = '𖤐𝗦𝗲́𝗻𝗮𝘁𝗲𝘂𝗿✍️𝗕𝗹𝗮𝗰𝗸✞𝗞𝗶𝗹𝗹𝗲𝗿'
 
 //console view/theme
 global.themeemoji = '🩵'
